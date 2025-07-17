22a1e8c8ab4c614e6a8d3525ea1294c850de1996
andreas793 <andreas7_93@mail.ru>
2025-07-17 18:46:06 +0300

    start

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,7 +19,7 @@ const About: FC<Props> = ({ generationTime }) =>  (
             </div>
             <div className="mt-8">
                 <Link
-                    href="/public"
+                    href="/"
                     className="text-blue-500 hover:underline"
                 >
                     Вернуться к списку продуктов
