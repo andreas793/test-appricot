95914c743fe40876befe3de33ed23027daa5047a
andreas793 <andreas7_93@mail.ru>
2025-07-17 17:56:30 +0300

    start

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -60,4 +60,12 @@ const ProductPage: FC<ProductPageProps> = async ({params}) => {
     }
 };
 
+export async function generateStaticParams(){
+    const response = await fetch('https://api.escuelajs.co/api/v1/products');
+    const products = await response.json();
+    return products.map((product: Product) => ({
+        id: product.id,
+    }));
+}
+
 export default ProductPage;
