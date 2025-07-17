35271ec3659986f4559a37a6448c1eccc06f33d6
andreas793 <andreas7_93@mail.ru>
2025-07-17 18:06:52 +0300

    start

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -61,11 +61,17 @@ const ProductPage: FC<ProductPageProps> = async ({params}) => {
 };
 
 export async function generateStaticParams(){
-    const response = await fetch('https://api.escuelajs.co/api/v1/products');
-    const products = await response.json();
-    return products.map((product: Product) => ({
-        id: product.id,
-    }));
+    try {
+        const res = await fetch('https://api.escuelajs.co/api/v1/products');
+        const products = await res.json();
+
+        return products.map((product: Product) => ({
+            id: String(product.id)
+        }));
+    } catch (_error) {
+        console.error('Error fetching products:', _error);
+        return [];
+    }
 }
 
 export default ProductPage;
